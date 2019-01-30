7b6966175da79bc13296848035e5e5b5e94581fa
Egor <egor_eremin@mail.ru>
2019-01-30 17:39:00 +0800

    adaptive index page 1024

--- build/js/main.js
+++ build/js/main.js
@@ -280,6 +280,30 @@ $(document).ready(function () {
       }
     });
   })();
+
+  (function toggleBurgerMenu() {
+    if ($('.burger-for-nav').length > 0) {
+      $('.burger-for-nav').on('click', function () {
+        if (!$(this).hasClass('active')) {
+          if ($(window).scrollTop() > 0) {
+            animatedScrollToPosition($('body, html'), 0, 300);
+            setTimeout(function () {
+              $('.burger-for-nav').addClass('active');
+            }, 300);
+          }
+
+          $('.burger-for-nav').addClass('active');
+          $('.header-main').addClass('fixed-position');
+          $('body').addClass('scroll-no');
+        } else {
+          $('.burger-for-nav').removeClass('active');
+          $('.header-main').removeClass('fixed-position');
+          $('body').removeClass('scroll-no');
+        } // $(this).toggleClass('active');
+
+      });
+    }
+  })();
 });
 
 function validationForm(formInit, textGood, textBad) {
