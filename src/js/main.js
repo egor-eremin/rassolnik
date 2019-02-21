a47b499bea489889c5fbc8dc92e8fd2d6d8385d3
Egor <egor_eremin@mail.ru>
2019-02-21 15:21:48 +0800

    finished reviews page

--- src/js/main.js
+++ src/js/main.js
@@ -437,6 +437,11 @@ $(document).ready(function () {
 			});
 		}
 	})();
+	(function activationTabs() {
+		if ($('.reviews').length > 0) {
+			addTabs('.reviews-block');
+		}
+	})()
 
 
 
@@ -549,4 +554,53 @@ function initMenuSlider(initSelector, selectorNavigationBlock, currentSlideSelec
 		// dots: true,
 		swipe: true,
 	});
+}
+function addTabs(tabbed_selector) {
+	var tabbed = document.querySelector(tabbed_selector);
+	var tablist = tabbed.querySelector('ul');
+	var tabs = tablist.querySelectorAll('a');
+	var panels = tabbed.querySelectorAll('[id^="section"]');
+	var switchTab = function switchTab(oldTab, newTab) {
+		newTab.focus();
+		newTab.removeAttribute('tabindex');
+		newTab.setAttribute('aria-selected', 'true');
+		oldTab.removeAttribute('aria-selected');
+		oldTab.setAttribute('tabindex', '-1');
+		var index = Array.prototype.indexOf.call(tabs, newTab);
+		var oldIndex = Array.prototype.indexOf.call(tabs, oldTab);
+		panels[oldIndex].hidden = true;
+		panels[index].hidden = false;
+	};
+	tablist.setAttribute('role', 'tablist');
+	Array.prototype.forEach.call(tabs, function (tab, i) {
+		tab.setAttribute('role', 'tab');
+		tab.setAttribute('id', 'tab' + (i + 1));
+		tab.setAttribute('tabindex', '-1');
+		tab.parentNode.setAttribute('role', 'presentation');
+		tab.addEventListener('click', function (e) {
+			e.preventDefault();
+			var currentTab = tablist.querySelector('[aria-selected]');
+			if (e.currentTarget !== currentTab) {
+				switchTab(currentTab, e.currentTarget);
+			}
+		});
+		tab.addEventListener('keydown', function (e) {
+			var index = Array.prototype.indexOf.call(tabs, e.currentTarget);
+			var dir = e.which === 37 ? index - 1 : e.which === 39 ? index + 1 : e.which === 40 ? 'down' : null;
+			if (dir !== null) {
+				e.preventDefault();
+				dir === 'down' ? panels[i].focus() : tabs[dir] ? switchTab(e.currentTarget, tabs[dir]) : void 0;
+			}
+		});
+	});
+	Array.prototype.forEach.call(panels, function (panel, i) {
+		panel.setAttribute('role', 'tabpanel');
+		panel.setAttribute('tabindex', '-1');
+		var id = panel.getAttribute('id');
+		panel.setAttribute('aria-labelledby', tabs[i].id);
+		panel.hidden = true;
+	});
+	tabs[0].removeAttribute('tabindex');
+	tabs[0].setAttribute('aria-selected', 'true');
+	panels[0].hidden = false;
 }
